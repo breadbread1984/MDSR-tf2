43bea629f171c3d0fbb69f7a7b5f65e7bebe966b
breadbread1984 <breadbread1984@163.com>
2021-10-15 13:35:09 +0800

    update

--- create_dataset.py
+++ create_dataset.py
@@ -15,7 +15,19 @@ def download():
   div2k_x3_builder.download_and_prepare();
   div2k_x4_builder.download_and_prepare();
 
-mean = [114.444 , 111.4605, 103.02  ]
+def parse_sample(features):
+  hr, lr = features['hr'], features['lr'];
+  lr = lr - tf.reshape([114.444 , 111.4605, 103.02  ], (1,1,3));
+  hr = hr - tf.reshape([114.444 , 111.4605, 103.02  ], (1,1,3));
+  return lr, hr;
+
+def load_datasets():
+  trainset_x2 = tfds.load(name = 'div2k/bicubic_x2', split = 'train', download = False);
+  testset_x2 = tfds.load(name = 'div2k/bicubic_x2', split = 'validation', download = False);
+  trainset_x3 = tfds.load(name = 'div2k/bicubic_x3', split = 'train', download = False);
+  testset_x3 = tfds.load(name = 'div2k/bicubic_x3', split = 'validation', download = False);
+  trainset_x4 = tfds.load(name = 'div2k/bicubic_x4', split = 'train', download = False);
+  testset_x4 = tfds.load(name = 'div2k/bicubic_x4', split = 'validation', download = False);
 
 if __name__ == "__main__":
   add_options();
